dddaf1c72529458ce8f1a0b674eeba96c0862ab8
Naomi <23469+norosa@users.noreply.github.com>
2021-09-26 01:04:04 +0000

    Add devtools variant

--- src/vscode/gh/brew/build.sh
+++ src/vscode/gh/brew/build.sh
@@ -1,37 +1,38 @@
 #!/bin/sh -e
 
-BREW="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
+
+# Install dependencies
+# -----------------------------------------------------------------------------
+
+apt-get update
+apt-get dist-upgrade -y
+
+apt-get install -y --no-install-recommends build-essential
+
+apt-get clean
+apt-get auto-remove
+rm -rf /var/lib/apt/lists/*
+
 
 # Install Homebrew
-curl -fsSL "${BREW}" | /bin/bash
+# -----------------------------------------------------------------------------
 
-# Set up Homebrew `shellenv` snippet
+BREW="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
+
+set -eo pipefail
+
+curl -fsSL "${BREW}" | /bin/bash 
+
 cat > /tmp/shellenv <<EOF
 
 eval "\$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 EOF
 
-# Copy the `shellenv` snippet to the system environment configuration, so that
-# `brew` is on the `PATH` for every user
-cat /tmp/shellenv >> /etc/environment
+cat /tmp/shellenv >> /home/.profile
+. /tmp/shellenv 
+rm /tmp/shellenv 
 
-# Source the `shellenv` snippet and then remove
-# shellcheck disable=SC1091
-. /tmp/shellenv
-rm /tmp/shellenv
-
-# Update packages
-apt-get update
-apt-get dist-upgrade -y
-
-# Install build tools
-apt-get install -y --no-install-recommends build-essential
+brew doctor
+brew update
 brew install gcc
-
-# Clean up APT
-apt-get clean
-apt-get auto-remove
-rm -rf /var/lib/apt/lists/*
-
-# Clean up Homebrew
 brew cleanup -v -s --prune=all
