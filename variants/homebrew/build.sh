fd471d46157b3be9f231cf1a93b16847ca8b22f3
Naomi <23469+norosa@users.noreply.github.com>
2021-09-26 12:26:35 +0000

    Improve container setups
    Specifically:
    
    - Pin the VS Code and Ubuntu versions so that they match each other.
    
    - The build commands for this image have been moved to `build.sh`. This
      file is then copied to the container, executed, and then removed.
      Doing this allows me to format the shell commands in a more readable
      way.
    
    - Both images now perform a `dist-upgrade`. Additionally, cleanup tasks
      have been expanded.
    
    - Previously, the `brew shellenv` command was added to `/etc/profile`.
      However, bdyefault,  this doesn't work with `sudo` because
      `/etc/profile` is only read for login shells. The result is that
      `sudo` cannot find `brew` on the `PATH`.
    
      To fix this, the `brew shellenv` has been appended to the
      `/etc/bash.bashrc` instead.

---
+++ variants/homebrew/build.sh
@@ -0,0 +1,39 @@
+#!/bin/sh -e
+
+BREW="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
+
+BUILD_ESSENTIAL="build-essential=12.8ubuntu1.1"
+
+# Install Homebrew
+curl -fsSL "${BREW}" | /bin/bash
+
+# Set up Homebrew `shellenv` snippet
+cat > /tmp/shellenv <<EOF
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+EOF
+
+# Copy the `shellenv` snippet to the system Bash configuration, so that `brew`
+# is on the `PATH` for every user
+cat /tmp/shellenv >> /etc/bash.bashrc
+
+# Source the `shellenv` snippet and then remove
+# shellcheck disable=SC1091
+. /tmp/shellenv
+rm /tmp/shellenv
+
+# Update packages
+apt-get update
+apt-get dist-upgrade -y
+
+# Install build tools
+apt-get install -y --no-install-recommends "${BUILD_ESSENTIAL}"
+brew install gcc
+
+# Clean up APT
+apt-get clean
+apt-get auto-remove
+rm -rf /var/lib/apt/lists/*
+
+# Clean up Homebrew
+brew cleanup -v -s --prune=all
