251c9ca208784bf832b91468898cd235421d0926
Naomi <23469+norosa@users.noreply.github.com>
2021-09-26 12:26:35 +0000

    Improve container setups
    Specifically:
    
    - Pin the VS Code and Ubuntu versions so that they match each other.
    
    - The build commands for this image have been moved to `build.sh`. This
      file is then copied to the container, executed, and then removed.
      Doing this allows me to format the shell commands in a more readable
      way.
    
    - Both images now perform a `dist-upgrade`. Additionally, cleanup tasks
      have been expanded.
    
    - Previously, the `brew shellenv` command was added to `/etc/profile`.
      However, bdyefault,  this doesn't work with `sudo` because
      `/etc/profile` is only read for login shells. The result is that
      `sudo` cannot find `brew` on the `PATH`.
    
      To fix this, the `brew shellenv` has been appended to the
      `/etc/bash.bashrc` instead.

---
+++ variants/base/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+# Update packages
+apt-get update
+apt-get dist-upgrade -y
+
+# Clean up APT
+apt-get clean
+apt-get auto-remove
+rm -rf /var/lib/apt/lists/*
+
+# Create a user for the GitHub-hosted runner
+localedef -i en_US -f UTF-8 en_US.UTF-8
+useradd -m -s /bin/bash runner
