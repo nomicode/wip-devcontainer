ae50858622068e16a1b2d30ade5388a25ea25485
Naomi Rose <23469+norosa@users.noreply.github.com>
2021-11-28 07:30:11 +0000

    Rework to use Alpine as base image

---
+++ src/universal/setup.sh
@@ -0,0 +1,45 @@
+#!/bin/ash -ex
+# shellcheck shell=dash
+
+# TODO: Turn this into a makefile so I can see what's happening during the
+# build
+
+apk update
+
+apk add --no-cache \
+    libffi-dev \
+    python3 \
+    python3-dev \
+    py3-pip \
+    black
+pip install --upgrade pip
+pip install pipx
+
+apk add --no-cache npm
+npm install --global npm@latest
+npm install --global --prefer-dedupe \
+    editorconfig \
+    cspell \
+    prettier \
+    dockerfilelint \
+    markdownlint-cli \
+    markdown-link-check \
+    stylelint \
+    stylelint-config-standard \
+    jscpd \
+    snyk
+
+echo "${PATH}"
+echo "${GOPATH}"
+
+apk add --no-cache go
+mkdir /usr/local/go
+go get -u github.com/get-woke/woke
+go get -u github.com/client9/misspell/cmd/misspell
+
+apk add --no-cache \
+    ruby \
+    tidyhtml \
+    github-cli \
+    shellcheck \
+    bash-completion
