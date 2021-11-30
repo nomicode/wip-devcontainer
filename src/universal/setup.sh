e8838875e8e80ecc481e4fdb7d72ad1a0f26b103
Naomi Rose <23469+norosa@users.noreply.github.com>
2021-11-30 19:19:59 +0000

    Add more packages to the universal tag

--- src/universal/setup.sh
+++ src/universal/setup.sh
@@ -1,10 +1,15 @@
-#!/bin/ash -ex
-# shellcheck shell=dash
+#!/bin/bash -e
 
 # TODO: Turn this into a makefile so I can see what's happening during the
 # build
 
 apk update
+
+apk add --no-cache \
+    coreutils \
+    gcc \
+    libc-dev \
+    make
 
 apk add --no-cache \
     libffi-dev \
@@ -15,6 +20,14 @@ apk add --no-cache \
 pip install --upgrade pip
 pip install pipx
 
+PIPX_BIN_DIR=/usr/local/bin
+export PIPX_BIN_DIR
+pipx install yamllint
+pipx install proselint
+pipx install snooty
+pipx install prospector[with_everything]
+pipx install reorder_python_imports
+
 apk add --no-cache npm
 npm install --global npm@latest
 npm install --global --prefer-dedupe \
@@ -22,11 +35,11 @@ npm install --global --prefer-dedupe \
     cspell \
     prettier \
     dockerfilelint \
-
     markdownlint-cli \
     markdown-link-check \
     stylelint \
     stylelint-config-standard \
+    pandoc \
     jscpd \
     snyk
 
@@ -35,13 +48,10 @@ npm install --global --prefer-dedupe \
 # https://github.com/drewbourne/vscode-remark-lint
 # https://github.com/remarkjs/remark-validate-links
 
-    # remark-cli \
-    # remark-lint \
-    # remark-preset-lint-markdown-style-guide \
-    # remark-stringify \
-
-echo "${PATH}"
-echo "${GOPATH}"
+# remark-cli \
+# remark-lint \
+# remark-preset-lint-markdown-style-guide \
+# remark-stringify \
 
 apk add --no-cache go
 mkdir /usr/local/go
@@ -49,8 +59,19 @@ go get -u github.com/get-woke/woke
 go get -u github.com/client9/misspell/cmd/misspell
 
 apk add --no-cache \
+    cargo
+cargo install --root=/usr/local shellharden
+
+apk add --no-cache \
+    ghc \
+    cabal
+cabal update
+cabal install --installdir=/usr/local/bin fix-whitespace
+
+apk add --no-cache \
     ruby \
     tidyhtml \
     github-cli \
     shellcheck \
+    shfmt \
     bash-completion
