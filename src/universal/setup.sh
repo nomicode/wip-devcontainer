c4ae2a0dc90bdf268877765cc6b23cc2194a87c5
Naomi Rose <23469+norosa@users.noreply.github.com>
2021-12-02 11:21:59 +0100

    Switch EditorConfig lint tools

--- src/universal/setup.sh
+++ src/universal/setup.sh
@@ -69,7 +69,6 @@ apk add --no-cache npm
 npm config set fund false --global
 npm install --global npm@latest
 npm install --global --prefer-dedupe \
-    editorconfig \
     cspell \
     prettier \
     dockerfilelint \
@@ -96,6 +95,7 @@ npm install --global --prefer-dedupe \
 # -----------------------------------------------------------------------------
 
 # mkdir "${GOPATH}"
+go get -u gitlab.com/greut/eclint/cmd/eclint
 go get -u github.com/get-woke/woke
 go get -u github.com/client9/misspell/cmd/misspell
 go get -u github.com/pksunkara/whitespaces
