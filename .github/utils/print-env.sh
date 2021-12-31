c4295a0f886412cf50196234c71dc6bd51627857
Naomi <23469+norosa@users.noreply.github.com>
2021-12-31 05:38:42 +0000

    Complete restructuring

---
+++ .github/utils/print-env.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -e
+
+key="${1}"
+
+if test -z "${key}"; then
+    exit 1
+fi
+
+image() {
+    echo "${key}" | sed 's,@.*,,' | sed 's,-,/,g'
+}
+
+tag() {
+    echo "${key}" | sed -E 's,[^@]+@?,,' | sed -E 's,^$,latest,'
+}
+
+context() {
+    image | while read -r path; do
+        find src -type d -path "*${path}"
+    done
+}
+
+cat <<EOF
+IMAGE=$(image)
+TAG=$(tag)
+CONTEXT=$(context)
+EOF
