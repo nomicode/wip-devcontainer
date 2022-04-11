66497bc6a2e12fd5e87b0aa4ac7a09f06ba4f318
Naomi <23469+nomiro@users.noreply.github.com>
2022-04-11 12:45:59 +0000

    fixup

--- src/apt-install.sh
+++ src/apt-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 apt-get update
-apt-get install -y --no-install-recommends <dpkg.txt
+xargs apt-get install -y --no-install-recommends <dpkg.txt
 apt-get clean -y
 dpkg -l | grep ii | awk '{print $2" "$3}' > dpkg-lock.txt
 rm -rf /var/lib/apt/lists/*
