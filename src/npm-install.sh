0f399452b0f61f80055b92b83140d0ca987a035e
Naomi <23469+nomiro@users.noreply.github.com>
2022-04-13 07:50:32 +0000

    fixup

--- src/npm-install.sh
+++ src/npm-install.sh
@@ -1,13 +1,5 @@
 #!/bin/sh -e
 
-DEBIAN_FRONTEND=noninteractive
-export DEBIAN_FRONTEND
-
-apt-get update
-apt-get install -y --no-install-recommends nodejs
-apt-get clean -y
-rm -rf /var/lib/apt/lists/*
-
 npm update
 npm install
 npm dedupe
