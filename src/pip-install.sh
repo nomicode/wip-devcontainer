4995d1115eacf8b94519c59b58962f36fa7bce5a
Naomi <23469+nomiro@users.noreply.github.com>
2022-04-13 15:24:36 +0000

    fixup

---
+++ src/pip-install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+
+DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND
+
+apt-get update
+apt-get install -y --no-install-recommends python3-venv
+apt-get clean -y
+rm -rf /var/lib/apt/lists/*
+
+python3 -m venv /opt/pip
+/opt/pip/bin/pip install -r requirements.txt
