dc77d94a32e2e1ff04bea4621713d92e062346c5
Naomi <23469+nomiro@users.noreply.github.com>
2022-04-13 15:54:56 +0000

    fixup

--- src/pip-install.sh
+++ src/pip-install.sh
@@ -6,7 +6,7 @@ export DEBIAN_FRONTEND
 apt-get update
 apt-get install -y --no-install-recommends python3-venv
 apt-get clean -y
-rm -rf /var/lib/apt/lists/*
+find /var/lib/apt/lists -mindepth 1 -delete
 
 python3 -m venv /opt/pip
 /opt/pip/bin/pip install -r requirements.txt
--- src/pipx-install.sh
+++ src/pipx-install.sh
@@ -10,4 +10,4 @@ find /var/lib/apt/lists -mindepth 1 -delete
 
 xargs -n 1 pipx install <requirements-pipx.txt
 
-pipx list | grep package | sed 's/,//'| awk '{print $2" "$3}' > pipx-lock.txt
+pipx list | grep package | sed 's, *package,,' > pipx-lock.txt
