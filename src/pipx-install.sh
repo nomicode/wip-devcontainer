cfe3192934beb185318c09227a50b6791c845c63
Naomi <23469+nomiro@users.noreply.github.com>
2022-04-13 16:06:34 +0000

    fixup

--- src/pipx-install.sh
+++ src/pipx-install.sh
@@ -4,10 +4,10 @@ DEBIAN_FRONTEND=noninteractive
 export DEBIAN_FRONTEND
 
 apt-get update -q
-apt-get install -qq -y --no-install-recommends python3-venv pipx
+apt-get install -q -y --no-install-recommends python3-venv pipx
 apt-get clean -y
 find /var/lib/apt/lists -mindepth 1 -delete
 
-xargs -n 1 pipx install <requirements-pipx.txt
+xargs -n 1 pipx install --system-site-packages <requirements-pipx.txt
 
 pipx list | grep package | sed 's, *package,,' > pipx-lock.txt
