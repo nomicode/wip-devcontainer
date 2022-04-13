e52c31885dccdb5959bf7782dbcad26fa1412228
Naomi <23469+nomiro@users.noreply.github.com>
2022-04-13 07:44:57 +0000

    fixup

--- src/apt-install.sh
+++ src/apt-install.sh
@@ -6,5 +6,6 @@ export DEBIAN_FRONTEND
 apt-get update
 xargs apt-get install -y --no-install-recommends <dpkg.txt
 apt-get clean -y
-dpkg -l | grep ii | awk '{print $2" "$3}' > dpkg-lock.txt
 rm -rf /var/lib/apt/lists/*
+
+dpkg -l | grep ii | awk '{print $2" "$3}' > dpkg-lock.txt
